7a839a354ebf5bd000974ab44b2fd2c295ee648d
xx-m-h-u-xx <56305452+xx-m-h-u-xx@users.noreply.github.com>
2021-05-18 12:54:46 +0100

    Update Program-01e-Plotting-Surface-Contour.py

--- Day-2-Tuesday-18-May-2021/Jupyter-Colab-Notebook-Exercise/Program-01e-Plotting-Surface-Contour.py
+++ Day-2-Tuesday-18-May-2021/Jupyter-Colab-Notebook-Exercise/Program-01e-Plotting-Surface-Contour.py
@@ -1,4 +1,4 @@
-''' Surface & Contour Plot '''
+'' Surface & Contour Plot '''
 # Program 01e: A program that plots a surface anmd contour plots in 3D
 import numpy as numpy
 import matplotlib.pyplot as plot
@@ -17,3 +17,20 @@ X,Y = np.meshgrid(phi_p, phi_m)
 Z = flux_qubit_potential (X,Y).T 
 
 fig = plt.figure(figsize=(8,6))
+ax=fig.add_subplot(1,1,1, projection='3d')
+p=ax.plot_wireframe(X,Y,Z, rstride=4, cstride=4)
+ax.plot_surface(X,Y,Z, rstride=4, cstride=4, alpha=0.25)
+
+cset = ax.contour(X,Y,Z, zdir='z', offset=-np.pi, cmap=plt.cm.coolwarm)
+cset = ax.contour(X,Y,Z, zdir='x', offset=-np.pi, cmap=plt.cm.coolwarm)
+cset = ax.contour(X,Y,Z, zdir='y', offset=3.np.pi, cmap=plt.cm.coolwarm)
+ax.set_xlim3d(-np.pi, 2*np.pi)
+ax.set_ylim3d(0, 3*np.pi)
+ax.set_zlim3d(-np.pi, 2*np.pi)
+ax.set_xlabel('$\phi_p$', fontsize=15)
+ax.set_ylabel('$\phi_m$' fontsize=15)
+ax.set_zlabel('Potential', fontsize=15)
+plt.tick_params(labelsize=15)
+ax.set_title("Surface and contour plots", fontsize=15)
+plt.show
+# displays a figure of a surface and contour plot
